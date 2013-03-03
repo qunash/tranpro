1237d6f91f993105bd1f94bd3308085f9e10d413
Anzor <andrey@192.168.124.50>
2013-03-03 23:08:35 +0400

    БОЛЬШОЕ ОБНОВЛЕНИЕ. Настроил стили для ВСЕХ вторичных элементов в главном активити (кроме основного tv с переводом)

--- TranslatePro/gen/com/qunash/translatepro/R.java
+++ TranslatePro/gen/com/qunash/translatepro/R.java
@@ -9,65 +9,212 @@ package com.qunash.translatepro;
 
 public final class R {
     public static final class array {
-        public static final int languages_list=0x7f040000;
+        public static final int languages_list=0x7f050000;
     }
     public static final class attr {
     }
     public static final class color {
-        public static final int pressed_example=0x7f050000;
+        public static final int azure_combo_acure=0x7f060001;
+        public static final int azure_combo_darker_gray=0x7f060002;
+        public static final int azure_combo_darker_red=0x7f060005;
+        public static final int azure_combo_lighter_gray=0x7f060003;
+        public static final int azure_combo_lighter_red=0x7f060004;
+        public static final int holo_focused_blue=0x7f060008;
+        public static final int holo_focused_lighter_blue=0x7f060006;
+        public static final int holo_pressed_blue=0x7f060009;
+        public static final int holo_pressed_lighter_blue=0x7f060007;
+        public static final int white=0x7f060000;
     }
     public static final class drawable {
         public static final int ab_bottom_solid_example=0x7f020000;
         public static final int ab_solid_example=0x7f020001;
         public static final int ab_stacked_solid_example=0x7f020002;
         public static final int ab_transparent_example=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int list_focused_example=0x7f020005;
-        public static final int menu_dropdown_panel_example=0x7f020006;
-        public static final int menu_hardkey_panel_example=0x7f020007;
-        public static final int pressed_background_example=0x7f020008;
-        public static final int progress_bg_example=0x7f020009;
-        public static final int progress_horizontal_example=0x7f02000a;
-        public static final int progress_primary_example=0x7f02000b;
-        public static final int progress_secondary_example=0x7f02000c;
-        public static final int selectable_background_example=0x7f02000d;
-        public static final int spinner_ab_default_example=0x7f02000e;
-        public static final int spinner_ab_disabled_example=0x7f02000f;
-        public static final int spinner_ab_focused_example=0x7f020010;
-        public static final int spinner_ab_pressed_example=0x7f020011;
-        public static final int spinner_background_ab_example=0x7f020012;
-        public static final int tab_indicator_ab_example=0x7f020013;
-        public static final int tab_selected_example=0x7f020014;
-        public static final int tab_selected_focused_example=0x7f020015;
-        public static final int tab_selected_pressed_example=0x7f020016;
-        public static final int tab_unselected_focused_example=0x7f020017;
-        public static final int tab_unselected_pressed_example=0x7f020018;
+        public static final int activated_background_holo_light=0x7f020004;
+        public static final int btn_check_holo_light=0x7f020005;
+        public static final int btn_check_off_disabled_focused_holo_light=0x7f020006;
+        public static final int btn_check_off_disabled_holo_light=0x7f020007;
+        public static final int btn_check_off_focused_holo_light=0x7f020008;
+        public static final int btn_check_off_holo_light=0x7f020009;
+        public static final int btn_check_off_pressed_holo_light=0x7f02000a;
+        public static final int btn_check_on_disabled_focused_holo_light=0x7f02000b;
+        public static final int btn_check_on_disabled_holo_light=0x7f02000c;
+        public static final int btn_check_on_focused_holo_light=0x7f02000d;
+        public static final int btn_check_on_holo_light=0x7f02000e;
+        public static final int btn_check_on_pressed_holo_light=0x7f02000f;
+        public static final int btn_default_disabled_focused_holo_light=0x7f020010;
+        public static final int btn_default_disabled_holo_light=0x7f020011;
+        public static final int btn_default_focused_holo_light=0x7f020012;
+        public static final int btn_default_holo_light=0x7f020013;
+        public static final int btn_default_normal_holo_light=0x7f020014;
+        public static final int btn_default_pressed_holo_light=0x7f020015;
+        public static final int btn_radio_holo_light=0x7f020016;
+        public static final int btn_radio_off_disabled_focused_holo_light=0x7f020017;
+        public static final int btn_radio_off_disabled_holo_light=0x7f020018;
+        public static final int btn_radio_off_focused_holo_light=0x7f020019;
+        public static final int btn_radio_off_holo_light=0x7f02001a;
+        public static final int btn_radio_off_pressed_holo_light=0x7f02001b;
+        public static final int btn_radio_on_disabled_focused_holo_light=0x7f02001c;
+        public static final int btn_radio_on_disabled_holo_light=0x7f02001d;
+        public static final int btn_radio_on_focused_holo_light=0x7f02001e;
+        public static final int btn_radio_on_holo_light=0x7f02001f;
+        public static final int btn_radio_on_pressed_holo_light=0x7f020020;
+        public static final int btn_rating_star_off_focused_holo_light=0x7f020021;
+        public static final int btn_rating_star_off_normal_holo_light=0x7f020022;
+        public static final int btn_rating_star_off_pressed_holo_light=0x7f020023;
+        public static final int btn_rating_star_on_focused_holo_light=0x7f020024;
+        public static final int btn_rating_star_on_normal_holo_light=0x7f020025;
+        public static final int btn_rating_star_on_pressed_holo_light=0x7f020026;
+        public static final int btn_toggle_holo_light=0x7f020027;
+        public static final int btn_toggle_off_disabled_focused_holo_light=0x7f020028;
+        public static final int btn_toggle_off_disabled_holo_light=0x7f020029;
+        public static final int btn_toggle_off_focused_holo_light=0x7f02002a;
+        public static final int btn_toggle_off_normal_holo_light=0x7f02002b;
+        public static final int btn_toggle_off_pressed_holo_light=0x7f02002c;
+        public static final int btn_toggle_on_disabled_focused_holo_light=0x7f02002d;
+        public static final int btn_toggle_on_disabled_holo_light=0x7f02002e;
+        public static final int btn_toggle_on_focused_holo_light=0x7f02002f;
+        public static final int btn_toggle_on_normal_holo_light=0x7f020030;
+        public static final int btn_toggle_on_pressed_holo_light=0x7f020031;
+        public static final int drawable_btn_clear=0x7f020032;
+        public static final int drawable_btn_go=0x7f020033;
+        public static final int drawable_ib_star=0x7f020034;
+        public static final int edit_text_holo_light=0x7f020035;
+        public static final int fastscroll_thumb_default_holo=0x7f020036;
+        public static final int fastscroll_thumb_holo=0x7f020037;
+        public static final int fastscroll_thumb_pressed_holo=0x7f020038;
+        public static final int ic_btn_go=0x7f020039;
+        public static final int ic_btn_go_inactive=0x7f02003a;
+        public static final int ic_clear=0x7f02003b;
+        public static final int ic_clear_inactive=0x7f02003c;
+        public static final int ic_go_inactive=0x7f02003d;
+        public static final int ic_launcher=0x7f02003e;
+        public static final int ic_menu_settings=0x7f02003f;
+        public static final int ic_speak_tts=0x7f020040;
+        public static final int ic_star_active=0x7f020041;
+        public static final int ic_star_inactive=0x7f020042;
+        public static final int ic_swap=0x7f020043;
+        public static final int image_button_background_my_styles=0x7f020044;
+        public static final int list_activated_holo=0x7f020045;
+        public static final int list_focused_example=0x7f020046;
+        public static final int list_focused_holo=0x7f020047;
+        public static final int list_longpressed_holo=0x7f020048;
+        public static final int list_pressed_holo_light=0x7f020049;
+        public static final int list_selector_background_transition_holo_light=0x7f02004a;
+        public static final int list_selector_disabled_holo_light=0x7f02004b;
+        public static final int list_selector_holo_light=0x7f02004c;
+        public static final int menu_dropdown_panel_example=0x7f02004d;
+        public static final int menu_hardkey_panel_example=0x7f02004e;
+        public static final int pressed_background_example=0x7f02004f;
+        public static final int progress_bg_example=0x7f020050;
+        public static final int progress_bg_holo_light=0x7f020051;
+        public static final int progress_horizontal_example=0x7f020052;
+        public static final int progress_horizontal_holo_light=0x7f020053;
+        public static final int progress_indeterminate_horizontal_holo_light=0x7f020054;
+        public static final int progress_primary_example=0x7f020055;
+        public static final int progress_primary_holo=0x7f020056;
+        public static final int progress_secondary_example=0x7f020057;
+        public static final int progress_secondary_holo=0x7f020058;
+        public static final int progress_small_holo=0x7f020059;
+        public static final int progressbar_indeterminate_holo1=0x7f02005a;
+        public static final int progressbar_indeterminate_holo2=0x7f02005b;
+        public static final int progressbar_indeterminate_holo3=0x7f02005c;
+        public static final int progressbar_indeterminate_holo4=0x7f02005d;
+        public static final int progressbar_indeterminate_holo5=0x7f02005e;
+        public static final int progressbar_indeterminate_holo6=0x7f02005f;
+        public static final int progressbar_indeterminate_holo7=0x7f020060;
+        public static final int progressbar_indeterminate_holo8=0x7f020061;
+        public static final int rate_star_big_half_holo_light=0x7f020062;
+        public static final int rate_star_big_off_holo_light=0x7f020063;
+        public static final int rate_star_big_on_holo_light=0x7f020064;
+        public static final int rate_star_small_half_holo_light=0x7f020065;
+        public static final int rate_star_small_off_holo_light=0x7f020066;
+        public static final int rate_star_small_on_holo_light=0x7f020067;
+        public static final int ratingbar_full_empty_holo_light=0x7f020068;
+        public static final int ratingbar_full_filled_holo_light=0x7f020069;
+        public static final int ratingbar_full_holo_light=0x7f02006a;
+        public static final int ratingbar_holo_light=0x7f02006b;
+        public static final int ratingbar_small_holo_light=0x7f02006c;
+        public static final int scrubber_control_disabled_holo=0x7f02006d;
+        public static final int scrubber_control_focused_holo=0x7f02006e;
+        public static final int scrubber_control_normal_holo=0x7f02006f;
+        public static final int scrubber_control_pressed_holo=0x7f020070;
+        public static final int scrubber_control_selector_holo_light=0x7f020071;
+        public static final int scrubber_primary_holo=0x7f020072;
+        public static final int scrubber_progress_horizontal_holo_light=0x7f020073;
+        public static final int scrubber_secondary_holo=0x7f020074;
+        public static final int scrubber_track_holo_light=0x7f020075;
+        public static final int selectable_background_example=0x7f020076;
+        public static final int spinner_16_inner_holo=0x7f020077;
+        public static final int spinner_16_outer_holo=0x7f020078;
+        public static final int spinner_ab_default_example=0x7f020079;
+        public static final int spinner_ab_disabled_example=0x7f02007a;
+        public static final int spinner_ab_focused_example=0x7f02007b;
+        public static final int spinner_ab_pressed_example=0x7f02007c;
+        public static final int spinner_background_ab_example=0x7f02007d;
+        public static final int spinner_background_holo_light=0x7f02007e;
+        public static final int spinner_default_holo_light=0x7f02007f;
+        public static final int spinner_disabled_holo_light=0x7f020080;
+        public static final int spinner_focused_holo_light=0x7f020081;
+        public static final int spinner_pressed_holo_light=0x7f020082;
+        public static final int style_ib_default_holo_light=0x7f020083;
+        public static final int style_ib_red_holo_light=0x7f020084;
+        public static final int style_ib_white_holo_light=0x7f020085;
+        public static final int switch_bg_disabled_holo_light=0x7f020086;
+        public static final int switch_bg_focused_holo_light=0x7f020087;
+        public static final int switch_bg_holo_light=0x7f020088;
+        public static final int switch_inner_holo_light=0x7f020089;
+        public static final int switch_thumb_activated_holo_light=0x7f02008a;
+        public static final int switch_thumb_disabled_holo_light=0x7f02008b;
+        public static final int switch_thumb_holo_light=0x7f02008c;
+        public static final int switch_thumb_pressed_holo_light=0x7f02008d;
+        public static final int switch_track_holo_light=0x7f02008e;
+        public static final int tab_indicator_ab_example=0x7f02008f;
+        public static final int tab_selected_example=0x7f020090;
+        public static final int tab_selected_focused_example=0x7f020091;
+        public static final int tab_selected_pressed_example=0x7f020092;
+        public static final int tab_unselected_focused_example=0x7f020093;
+        public static final int tab_unselected_pressed_example=0x7f020094;
+        public static final int textfield_activated_holo_light=0x7f020095;
+        public static final int textfield_default_holo_light=0x7f020096;
+        public static final int textfield_disabled_focused_holo_light=0x7f020097;
+        public static final int textfield_disabled_holo_light=0x7f020098;
+        public static final int textfield_focused_holo_light=0x7f020099;
     }
     public static final class id {
-        public static final int btn01=0x7f090006;
-        public static final int et01=0x7f090005;
-        public static final int imgbtnSwitchLanguages=0x7f090001;
-        public static final int listView1=0x7f090007;
-        public static final int menu_languages=0x7f090008;
-        public static final int spSL=0x7f090000;
-        public static final int spTL=0x7f090002;
-        public static final int sv01=0x7f090003;
-        public static final int tv01=0x7f090004;
+        public static final int et01=0x7f090008;
+        public static final int ibClear=0x7f090007;
+        public static final int ibSwapLanguages=0x7f090003;
+        public static final int ibTranslate=0x7f090009;
+        public static final int menu_settings=0x7f09000a;
+        public static final int spSL=0x7f090002;
+        public static final int spTL=0x7f090004;
+        public static final int sv01=0x7f090005;
+        public static final int tv01=0x7f090006;
+        public static final int tvLanguageName=0x7f090000;
+        public static final int tvSourcePhrase=0x7f090001;
     }
     public static final class layout {
-        public static final int languages_activity=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int test=0x7f030002;
+        public static final int layout_divider=0x7f030000;
+        public static final int layout_language_name=0x7f030001;
+        public static final int layout_source_phrase=0x7f030002;
+        public static final int layout_speak_button=0x7f030003;
+        public static final int layout_star=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int test=0x7f030006;
     }
     public static final class menu {
         public static final int mymenu=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int btnTranslate=0x7f060002;
-        public static final int etTranslateHint=0x7f060003;
-        public static final int menu_settings=0x7f060001;
-        public static final int switch_languages=0x7f060004;
+        public static final int app_name=0x7f070000;
+        public static final int et_TranslateHint=0x7f070002;
+        public static final int ib_clear=0x7f070005;
+        public static final int ib_speak=0x7f070007;
+        public static final int ib_swap_languages=0x7f070003;
+        public static final int ib_translate=0x7f070004;
+        public static final int menu_settings=0x7f070001;
+        public static final int settings=0x7f070006;
     }
     public static final class style {
         /** 
@@ -91,10 +238,32 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
-        /**  Application theme. 
+        public static final int AppBaseTheme=0x7f040014;
+        /**      <item name="android:progress">@style/ProgressBarSmallAppTheme</item> 
+ Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f040013;
+        public static final int AutoCompleteTextViewAppTheme=0x7f040001;
+        public static final int ButtonAppTheme=0x7f040004;
+        public static final int CheckBoxAppTheme=0x7f040002;
+        public static final int EditTextAppTheme=0x7f040000;
+        public static final int ImageButtonAppTheme=0x7f040005;
+        public static final int ListViewAppTheme=0x7f04000e;
+        public static final int ListViewAppTheme_White=0x7f04000f;
+        public static final int ProgressBarAppTheme=0x7f040008;
+        public static final int RadioButtonAppTheme=0x7f040003;
+        public static final int RatingBarAppTheme=0x7f04000a;
+        public static final int RatingBarBigAppTheme=0x7f04000b;
+        public static final int RatingBarSmallAppTheme=0x7f04000c;
+        public static final int SeekBarAppTheme=0x7f040009;
+        public static final int SpinnerAppTheme=0x7f040006;
+        public static final int SpinnerDropDownItemAppTheme=0x7f040007;
+        public static final int SpinnerItemAppTheme=0x7f040010;
+        public static final int ToggleAppTheme=0x7f04000d;
+        /**  My styles 
+         */
+        public static final int style_tvLanguageName=0x7f040011;
+        public static final int style_tvTranslation=0x7f040012;
     }
 }
--- TranslatePro/src/com/qunash/translatepro/Main.java
+++ TranslatePro/src/com/qunash/translatepro/Main.java
@@ -8,24 +8,41 @@ import android.os.Bundle;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.text.Editable;
 import android.text.Html;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class Main extends ParentActivity {
 
-	private static String LOG_TAG = "TranslatePro";
+	//private static String LOG_TAG = "TranslatePro";
 	
 	
 	EditText et01 = null;
+	static ScrollView sv01 = null;
 	static TextView tv01 = null;
-	static Button btnTranslate = null;
+	static ImageButton ibTranslate = null;
+	Spinner spSL;
+	Spinner spTL;
+	ImageButton ibSwapLanguages;
+	ImageButton ibClear;
+	ArrayAdapter<CharSequence> adapter;
+	
+	String sl;
+	String tl;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +60,7 @@ public class Main extends ParentActivity {
 //        page = inflater.inflate(R.layout.main, null);
 //        et01 = (EditText) page.findViewById(R.id.et01);
 //		tv01 = (TextView) page.findViewById(R.id.tv01);
-//		btnTranslate = (Button) page.findViewById(R.id.btn01);
+//		ibTranslate = (Button) page.findViewById(R.id.btn01);
 //		
 //		Typeface tf = Typeface.createFromAsset(getAssets(), "SegoeWP-Semilight.ttf");
 //		tv01.setTypeface(tf);
@@ -61,22 +78,123 @@ public class Main extends ParentActivity {
 //		//****************//
 		
 		setContentView(R.layout.main);
+
+		ibClear = (ImageButton) findViewById(R.id.ibClear);
+		ibClear.setEnabled(false); //TODO remove this hardcode (for some reason, isEnabled() of this imageButton = "true" even isClickable set to "false")
+		
+		ibTranslate = (ImageButton) findViewById(R.id.ibTranslate);
+		ibTranslate.setEnabled(false); //TODO remove this hardcode (for some reason this imageButton isEnabled() = "true" even isClickable set to "false")
 		
 		et01 = (EditText) findViewById(R.id.et01);
+		et01.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+					ibClear.setEnabled(s.length()>0 ? true : false);
+					ibTranslate.setEnabled(s.length()>0 ? true : false);
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+			
+			@Override
+			public void afterTextChanged(Editable s) {}
+		});
+		
+		et01.setOnFocusChangeListener(new OnFocusChangeListener() {
+			
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (!hasFocus) {
+					// hide soft keyboard
+					InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+					inputManager.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+					et01.clearFocus();
+				}
+			}
+		});
+		
+		sv01 = (ScrollView) findViewById(R.id.sv01);
 		tv01 = (TextView) findViewById(R.id.tv01);
-		btnTranslate = (Button) findViewById(R.id.btn01);
 		
 		Typeface tf = Typeface.createFromAsset(getAssets(), "SegoeWP-Semilight.ttf");
 		tv01.setTypeface(tf);
 		
+		// spinners
+		spSL = (Spinner) findViewById(R.id.spSL);
+		spTL = (Spinner) findViewById(R.id.spTL);
+		ibSwapLanguages = (ImageButton) findViewById(R.id.ibSwapLanguages);
+		
+		
+		adapter = ArrayAdapter.createFromResource(this, R.array.languages_list, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		sl = mPreferences.getString("sl", adapter.getPosition(mCurrentLanguage)>-1 ? mCurrentLanguage : "en");
+		tl = mPreferences.getString("tl", "en");
+		
+		spSL.setAdapter(adapter);
+		spSL.setSelection(adapter.getPosition(sl));
+		spSL.setOnItemSelectedListener(new listener());
+		
+		spTL.setAdapter(adapter);
+		spTL.setSelection(adapter.getPosition(tl));
+		spTL.setOnItemSelectedListener(new listener());
+		
+	}
+	
+	public class listener implements OnItemSelectedListener {
+
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+			
+			switch (parent.getId()) {
+			case R.id.spSL:
+				String sl = parent.getItemAtPosition(position).toString();	
+				
+				mPrefEditor.set("sl", sl);
+				break;
+			
+			case R.id.spTL:
+				String tl = parent.getItemAtPosition(position).toString();	
+				
+				mPrefEditor.set("tl", tl);
+				break;
+			
+			default:
+				break;
+			}
+			
+			
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> parent) {
+
+		}
+
 	}
 
-	public void Translate(View view) {
+	public void SwapLanguages(View view) {
 		
-		// hide soft keyboard
-		InputMethodManager inputManager = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
-		inputManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-		et01.clearFocus();
+		String buffer = sl;
+		
+		sl = tl;
+		tl = buffer;
+		
+		buffer = null;
+		
+		spSL.setSelection(adapter.getPosition(sl));
+		spTL.setSelection(adapter.getPosition(tl));
+
+		mPrefEditor.set("sl", sl);
+		mPrefEditor.set("tl", tl);
+	
+	}
+	
+	public void ClearText(View view) {
+		et01.setText("");
+	}
+	public void Translate(View view) {
 		
 		// Check internet connection
 		if (!hasConnection()) {
@@ -85,7 +203,7 @@ public class Main extends ParentActivity {
 		}
 		
 		// make button inactive
-		btnTranslate.setEnabled(false);
+		ibTranslate.setEnabled(false);
 		
 		String tl = mPreferences.getString("tl", "");
 		String sl = mPreferences.getString("sl", "");
@@ -120,10 +238,11 @@ public class Main extends ParentActivity {
 	public static void showResult(String output) {
 		
 		// reactivate button
-		btnTranslate.setEnabled(true);
+		ibTranslate.setEnabled(true);
 		
 		tv01.setText(Html.fromHtml(output));
 		
+		sv01.fullScroll(ScrollView.FOCUS_UP);
 	}
 	
 	@Override
@@ -138,8 +257,8 @@ public class Main extends ParentActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
     	switch (item.getItemId()) {
-		case R.id.menu_languages:
-			Intent intent = new Intent(this, LanguagesActivity.class);
+		case R.id.menu_settings:
+			Intent intent = new Intent(this, SettingsActivity.class);
 			startActivity(intent);
 			break;
 
